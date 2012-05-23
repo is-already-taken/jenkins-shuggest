81123fe0dbd5e4b4b795ed5d6dbd0b54ec72321f
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2012-05-24 00:39:32 +0200

    Added index script to generate index.json for Shuggest
    - The script uses find to search for files in $JENKINS_HOME/userContent
    - One can append find options to narrow the search

---
+++ index-usercontent.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Author Thomas Lehmann, May 2012
+# 
+
+USERCONTENT_PATH="${JENKINS_HOME}/userContent"
+INDEX_FILENAME="index.json"
+
+BINNAME="`basename $0`"
+
+function help(){
+	cat <<EOH
+Generate index of Jenkins' userContent/ directory.
+
+Usage: $BINNAME [<find options>]
+
+Synopsis
+    This script will generate a index of Jenkins userContent directory in form
+    of a JSON file. The file can then be used the "shuggest" Userscript to show 
+    a list of shell scripts that are executable in jobs next to a shell textarea.
+
+    Script files may contain comments with a short description and usage text.
+
+        # usercontent-index.descr=Description goes here
+        # usercontent-index.usage=Usage goes here
+
+Options
+    find options      Options to narrow find's search results.
+                      You could for example exclude test.sh with
+
+                          $ $BINNAME ! -name "test.sh"
+
+EOH
+}
+
+if [[ $1 =~ ^-(-help|h|\?)$ ]]; then
+	help
+	exit 0
+fi
+
+if [ -z "${JENKINS_HOME}" ]; then
+	echo "JENKINS_HOME not set. Are you Jenkins user? Try whoami or check your shell."
+	exit 1
+fi
+
+if [ ! -d "${JENKINS_HOME}" ]; then
+	echo "JENKINS_HOME does not point to a directory. Check your Jenkins setup."
+	exit 1
+fi
+
+if [ ! -d $USERCONTENT_PATH ]; then
+	echo "Jenkins userContent is not a directory or not existing: $USERCONTENT_PATH" >/dev/stderr
+	exit 1
+fi
+
+FIND_OPTS="$*"
+
+
+JSON="["
+
+for F in `find $USERCONTENT_PATH -type f ! -name "readme.txt" ! -name "index.json" $FIND_OPTS | sed 's+.*userContent/++g' 2>/dev/null`; do 
+	DESCR="`cat $USERCONTENT_PATH/$F | grep 'usercontent-index.descr' | sed 's+^#[^=]*=\(.*\)$+\1+'`"
+	USAGE="`cat $USERCONTENT_PATH/$F | grep 'usercontent-index.usage' | sed 's+^#[^=]*=\(.*\)$+\1+'`"
+
+	JSON="$JSON{"path": \"$F\", "description": \"$DESCR\", "usage": \"$USAGE\"},"
+done
+
+JSON="`echo $JSON | sed 's+,$++'`]"
+
+echo "$JSON" > $INDEX_FILENAME
+
+echo "$INDEX_FILENAME written."
