d4bc6430a2a9bdb757f4b3e1eee99460df4b9ba1
Thomas Lehmann <t.lehmann@strato-rz.de>
2012-06-15 13:32:51 +0200

    Fixed wrong entity replacement order: replacing & before everything else

--- shuggest.js
+++ shuggest.js
@@ -32,7 +32,7 @@
 		shuggestData = null;
 
 	function escapeHtml(str){
-		return str.replace(/"/g, "&quot;").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+		return str.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 	}
 	
 	function renderDetail(str, cls){
